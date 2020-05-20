84bd9312ec7424fef96d564cee4faa001f74384d
Chandra Rudrakshala <chandra.rudrakshala@marriott.com>
2020-05-20 12:44:59 -0400

    updated tests with static string

--- src/test/java/org/tqa/AppTest.java
+++ src/test/java/org/tqa/AppTest.java
@@ -7,33 +7,35 @@ import static org.junit.Assert.*;
 /**
  * Unit test for Sentence App.
  */
-public class AppTest 
+public class AppTest
 {
+    private static String sentence = "The cow jumped over the moon.";
+
     @Test
     public void getLongestWordTest()
     {
-        App app = new App("The cow jumped over the moon.");
+        App app = new App(sentence);
         assertEquals(app.longestWord(), "jumped");
     }
 
     @Test
     public void getShortestWordTest()
     {
-        App app = new App("The cow jumped over the moon.");
+        App app = new App(sentence);
         assertEquals(app.shortestWord(), "The");
     }
 
     @Test
     public void getShortestWordLengthTest()
     {
-        App app = new App("The cow jumped over the moon.");
+        App app = new App(sentence);
         assertEquals(app.getShortestWordLength(), 3);
     }
 
     @Test
     public void getLongestWordLengthTest()
     {
-        App app = new App("The cow jumped over the moon.");
+        App app = new App(sentence);
         assertEquals(app.getLongestWordLength(), 6);
     }
 }
